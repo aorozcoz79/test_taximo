d65a4d4060bafe193369d467df7732de9cedb279
unknown <aorozcoz79@gmail.com>
2020-11-12 09:20:29 -0500

    ajustando controller

--- controllers/calcular.controller.js
+++ controllers/calcular.controller.js
@@ -27,7 +27,7 @@ const control = {
     // Buscar registro de la matriz
     getByFind: async (datos) => {
         try {
-            console.log('Verifica si existe la secuencia de la matriz');
+            console.log('Verifica si existe la secuencia de la matriz dada');
             const { parameters, shoping_centers, roads } = datos
             const dato = await Datos.findOne({
                 where: {
@@ -44,7 +44,47 @@ const control = {
     minimum_time: async (datos) => {
         try {
             console.log('Calcula el valor minimun_time');
-            datos.minimum_time = 50
+            
+
+            // Obtiene los valores de entrada
+            const { parameters, shoping_centers, roads } = datos
+
+            // crea un array de datos de los parametros
+            let parameters2 = parameters.split(',')
+            console.log(parameters2);
+            // crea un array de datos de los centros comerciales
+            shoping_centers2 = shoping_centers.split('-')
+            console.log(shoping_centers2);
+            // crea un array de datos de los carreteras
+            roads2 = roads.split('-')
+            console.log(roads2);
+
+
+            let n, m, k ;
+            // Asigna los valores de n, k y m segun la variable parametro
+            n = parameters2[0]
+            m = parameters2[1]
+            k = parameters2[2]
+            console.log(n, m, k);
+
+            // Restricciones
+            if (2 > n && n > 1000) {
+                return { status: false, message: 'Falló al crear el registro', data: {} }
+            }
+
+            if (1 > m && m > 2000) {
+                return { status: false, message: 'Falló al crear el registro', data: {} }
+            }
+
+            if (1 > k && k > 10) {
+                return { status: false, message: 'Falló al crear el registro', data: {} }
+            }
+
+            
+
+            var aleatorio =  Math.round((Math.random()*100)/10)*10;
+            console.log(aleatorio);
+            datos.minimum_time = aleatorio
             return await control.create(datos)
         } catch (e) {
             
