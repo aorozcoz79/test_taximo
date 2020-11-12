84f666ab07239b52bd492339830e9cb38f10acb4
unknown <aorozcoz79@gmail.com>
2020-11-11 21:32:18 -0500

    ajustando conexion BD heroku

--- database/database.js
+++ database/database.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-    'test_taximo',
-    'taximo_pg',
-    '123456',
+    'dlvuokooed8qd',
+    'diseoyadidluzk',
+    '5070e3afd18c377b7f6977076908315d130982b9c0720c127c0a557d61e9636e',
     {        
-        host: '35.238.242.156',
+        host: 'ec2-34-237-247-76.compute-1.amazonaws.com',
         dialect: 'postgres',
         pool: {
             max: 5,
